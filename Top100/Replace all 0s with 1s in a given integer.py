a4de77f4f7213bd2b6e2d8648c3b8433ca1dc17b
Kalparatna <kalparatna223@gmail.com>
2025-06-24 09:21:08 +0530

    Top100/Replace all 0s with 1s in a given integer.py

---
+++ Top100/Replace all 0s with 1s in a given integer.py
@@ -0,0 +1,19 @@
+
+
+def zeroTone(n):
+    return int(str(n).replace("0", "1"))
+
+def withoutfuntion(n):
+    strnum = str(n)
+    result = ""
+    for digit in strnum:
+        if digit == "0":
+            result += "1"
+        else:
+            result += digit
+    return int(result)
+
+if __name__ == "__main__":
+
+    num = 110340
+    print(withoutfuntion(num))
