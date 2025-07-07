44446dfdf21859c9ce60d40f82b14808eb0a462b
Kalparatna <kalparatna223@gmail.com>
2025-07-07 11:47:29 +0530

    count_number_with_duplicate.py

--- count_number_with_duplicate.py
+++ count_number_with_duplicate.py
@@ -1,14 +1,10 @@
 '''
 Problem Description -:  Given two non-negative integers n1 and n2, where n1
-
 For example:
-
 Suppose n1=11 and n2=15.
-
 There is the number 11, which has repeated digits, but 12, 13, 14 and 15 have no repeated digits. So, the output is 4.
 
 Example1:
-
 Input:
 
 11 — Vlaue of n1
@@ -32,8 +28,8 @@ Output:
 n1 = int(input())
 n2 = int(input())
 
-c= 0
+count= 0
 for i in range(n1, n2+1):
     if len(set(str(i))) == len(str(i)):
-        c += 1
-print(c)
+        count += 1
+print(count)
